3ab11847c910ac7be16bab74a6aa13c97ea94764
richfjiq <rfjiq1986@gmail.com>
2022-06-14 07:17:29 -0500

    chore: add Navbar

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,26 @@
+import { FC, ReactNode } from 'react';
+import Head from 'next/head';
+import { Box } from '@mui/material';
+
+import { Navbar } from '../ui';
+
+interface Props {
+  title?: string;
+  children: ReactNode;
+}
+
+export const Layout: FC<Props> = ({ title = 'OpenJira', children }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navbar />
+
+      {/* Navbar */}
+      {/* Sidebar */}
+
+      <Box sx={{ padding: '10px 20px' }}>{children}</Box>
+    </Box>
+  );
+};
